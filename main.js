1268fea8544cd31e2d54261a601e7aea4524831d
yusuf umar faruq <luwaservices@gmail.com>
2023-06-24 21:22:40 -0700

    code refactoring

--- main.js
+++ main.js
@@ -1,24 +1,70 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const mobileNav = document.querySelector('.navigation')
+const navToggle = document.querySelector('.mobile-navigation-toggle');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// LISTEN FOR A CLICK EVENT ON MOBILE MENU TOGGLE
+navToggle.addEventListener('click', () => {
+    mobileNav.classList.toggle('active');
+})
 
-setupCounter(document.querySelector('#counter'))
+const form = document.querySelector('#form')
+let userInputField = document.querySelector('.input');
+let parent = document.querySelector('#parent');
+let urlList = document.querySelector('.url-list');
+
+const shortenUrlLink  = async () => {
+    try{
+        let originalLink = userInputField.value;
+        if (originalLink === "") {
+            errorMessage();
+        }else {
+            let urlLink = `https://api.shrtco.de/v2/shorten?url=${originalLink}`;
+            const response = await axios.get(urlLink)
+            createListElement(response, originalLink);
+        }
+    }
+
+    catch(e) {
+        console.log('Error:', e)
+    }
+}
+
+const errorMessage = () => {
+     userInputField.setAttribute('placeholder', 'Input field is empty!');
+}
+
+const createListElement = (response, originalLink) => {
+    let newLi = document.createElement('li');
+    newLi.className = 'new-list';
+
+    let div = document.createElement('div');
+    div.className = 'list-copy-container';
+    div.innerHTML = `<a href="${response.data.result.full_short_link}" target="_blank">
+    ${response.data.result.full_short_link}</a><button class="copy-button" data-clipboard-text="${response.data.result.full_short_link}</">copy</button>`
+
+    newLi.append(originalLink, div);
+    parent.prepend(newLi);
+    userInputField.value = '';
+
+    let copyButton = document.querySelector('.copy-button');
+    copyShortenedUrl(copyButton)
+}
+
+//copy url to users clipboard when copyButton is clicked
+const copyShortenedUrl = (copyButton) => {
+    copyButton.addEventListener('click', () => {
+        new ClipboardJS('copybutton');
+        copyButton.innerText = 'copied!';
+    })
+}
+
+//when user clicks on submit button, shorten provided url
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    shortenUrlLink();
+})
+
+// observations
+// when using async await with the shrtcode api, its gives an error message (CORS preflight did not succeed)
+// async function apicall() {
+
+// }
